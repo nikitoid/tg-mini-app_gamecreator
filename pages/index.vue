772e4657177b503b3534af0155e388505cae6b7e
nikitoid <neekeetoeed@gmail.com>
2024-12-26 15:53:12 +0400

    First app for test

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  const { MainButton, useWebAppPopup, useWebApp } = await import('vue-tg')
+  
+  const { showAlert } = useWebAppPopup()
+
+  const { initDataUnsafe } = useWebApp()
+</script>
+
+<template>
+    <section>
+        <h1>Hello Telegram Mini App Game Creator</h1>
+
+        <h2> Ваше имя это {{initDataUnsafe.user?.username}} </h2>
+
+        <MainButton text="Вот она я" color="rgba(0, 191,111,1)" @click="() => showAlert('А вот и сообщение')" />
+    </section>
+</template>
+
+<style scoped>
+
+
+</style>
